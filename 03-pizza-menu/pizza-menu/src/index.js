a0e352329d0828df2d8dcf8c3bdba82921ba8377
Leah Wan <leahw7@protonmail.com>
2024-08-26 17:12:55 -0400

    conditional rendering with ternary operators

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -80,7 +80,7 @@ function Menu() {
         <main className="menu">
             <h2>Our Menu</h2>
 
-            {numPizzas > 0 && (
+            {numPizzas > 0 ? (
                 <ul className='pizzas'>
                     {pizzas.map((pizza) => (
                         <Pizza
@@ -89,7 +89,7 @@ function Menu() {
                         />
                     ))}
                 </ul>
-            )}
+            ) : <p>We're still working on our menu. Please come back later :)</p>}
 
         </main>
     )
@@ -117,12 +117,13 @@ function Footer() {
 
     return (
         <footer className="footer">
-            {isOpen && (
+            {isOpen ? (
                 <div className='order'>
                     <p>We're Open until {closeHour}:00. Come visit us or order online.</p>
                     <button className='btn'>Order</button>
                 </div>
-            )}
+            )
+                : <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00!</p>}
         </footer>
     )
 }
