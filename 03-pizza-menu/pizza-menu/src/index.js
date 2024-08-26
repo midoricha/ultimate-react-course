bc2bd509added1f7f4be109a6c42b0c3414ccf33
Leah Wan <leahw7@protonmail.com>
2024-08-26 17:08:01 -0400

    conditional rendering with &&

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -73,18 +73,24 @@ function Header() {
 }
 
 function Menu() {
+    const pizzas = pizzaData;
+    const numPizzas = pizzas.length;
+
     return (
         <main className="menu">
             <h2>Our Menu</h2>
 
-            <ul className='pizzas'>
-                {pizzaData.map((pizza) => (
-                    <Pizza
-                        pizzaObj={pizza}
-                        key={pizza.name}
-                    />
-                ))}
-            </ul>
+            {numPizzas > 0 && (
+                <ul className='pizzas'>
+                    {pizzas.map((pizza) => (
+                        <Pizza
+                            pizzaObj={pizza}
+                            key={pizza.name}
+                        />
+                    ))}
+                </ul>
+            )}
+
         </main>
     )
 }
@@ -111,7 +117,12 @@ function Footer() {
 
     return (
         <footer className="footer">
-            {new Date().toLocaleTimeString()} We're currently open
+            {isOpen && (
+                <div className='order'>
+                    <p>We're Open until {closeHour}:00. Come visit us or order online.</p>
+                    <button className='btn'>Order</button>
+                </div>
+            )}
         </footer>
     )
 }
