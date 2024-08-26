876de27ba78a16b06b2984cd393778b47335752f
Leah Wan <leahw7@protonmail.com>
2024-08-26 18:39:17 -0400

    props destructure; React fragment

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -80,33 +80,38 @@ function Menu() {
         <main className="menu">
             <h2>Our Menu</h2>
 
+
             {numPizzas > 0 ? (
-                <ul className='pizzas'>
-                    {pizzas.map((pizza) => (
-                        <Pizza
-                            pizzaObj={pizza}
-                            key={pizza.name}
-                        />
-                    ))}
-                </ul>
+                <>
+                    <p>
+                        Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.
+                    </p>
+                    <ul className='pizzas'>
+                        {pizzas.map((pizza) => (
+                            <Pizza
+                                pizzaObj={pizza}
+                                key={pizza.name}
+                            />
+                        ))}
+                    </ul>
+                </>
             ) : <p>We're still working on our menu. Please come back later :)</p>}
 
         </main>
     )
 }
 
-function Pizza(props) {
-    console.log(props);
+function Pizza({ pizzaObj }) {
 
-    if (props.pizzaObj.soldOut) return null;
+    if (pizzaObj.soldOut) return null;
 
     return (
         <li className="pizza">
-            <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+            <img src={pizzaObj.photoName} alt={pizzaObj.name} />
             <div>
-                <h3>{props.pizzaObj.name}</h3>
-                <p>{props.pizzaObj.ingredient}</p>
-                <span>{props.pizzaObj.price}</span>
+                <h3>{pizzaObj.name}</h3>
+                <p>{pizzaObj.ingredient}</p>
+                <span>{pizzaObj.price}</span>
             </div>
         </li>
     );
@@ -118,17 +123,23 @@ function Footer() {
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
 
-    if (!isOpen) return (<p>We're happy to welcome you between {openHour}:00 and {closeHour}:00!</p>);
-
     return (
         <footer className="footer">
-
-            <div className='order'>
-                <p>We're Open until {closeHour}:00. Come visit us or order online.</p>
-                <button className='btn'>Order</button>
-            </div>
-
+            {isOpen ? (
+                <Order closeHour={closeHour} />
+            ) : (
+                <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00!</p>
+            )}
         </footer>
+    )
+}
+
+function Order({ closeHour }) {
+    return (
+        <div className='order'>
+            <p>We're Open until {closeHour}:00. Come visit us or order online.</p>
+            <button className='btn'>Order</button>
+        </div>
     )
 }
 
